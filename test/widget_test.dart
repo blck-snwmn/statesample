c62a8b232e1fa085b1e2d3f5d43c05e2e8db9310
blck-snwmn <whitesnowmancreator@gmail.com>
2022-11-09 23:59:28 +0900

    Add golden test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,19 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:statesample/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testGoldens('sample_golden_test', (WidgetTester tester) async {
+    await loadAppFonts();
+    //デバイスの画面サイズ(iPhone6)
+    const size6 = Size(375, 667);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.pumpWidgetBuilder(const ProviderScope(child: MyApp()),
+        surfaceSize: size6);
+    await screenMatchesGolden(tester, 'sample_iphone6');
   });
 }
